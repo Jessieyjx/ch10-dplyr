ea6a1aa19a05dbca05c505db8f5987d43e04cd5b
YANG JUNXI <yangjunxi@D-69-91-140-212.dhcp4.washington.edu>
2018-04-11 17:15:18 -0700

    Update exercise

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -11,16 +11,59 @@ library(fueleconomy)
 
 # Which 2015 Acura model has the best hwy MGH? (Use dplyr, but without method
 # chaining or pipes--use temporary variables!)
-
+acura_2015 <- filter(vehicles, make == 'Acura', year == 2015)
+acura_best <- filter(acura_2015, hwy == max(hwy))
+best_acura_model <- select(acura_best, model)
 
 # Which 2015 Acura model has the best hwy MPG? (Use dplyr, nesting functions)
-
+best_acura_model <- select(
+  filter(
+    filter(
+      vehicles,
+      make == 'Acura',
+      year == 2015
+    ),
+    hwy == max(hwy)
+  ),
+  model
+)
 
 # Which 2015 Acura model has the best hwy MPG? (Use dplyr and the pipe operator)
-
+best_acura_model <- filter(vehicles, make == 'Acura', year == 2015) %>%
+  filter(hwy == max(hwy)) %>%
+  select(model)
 
 
 ### Bonus
 
 # Write 3 functions, one for each approach.  Then,
 # Test how long it takes to perform each one 1000 times
+temp_best_acura_model <- function() {
+  acura_2015 <- filter(vehicles, make == 'Acura', year == 2015)
+  acura_best <- filter(acura_2015, hwy == max(hwy))
+  best_acura_model <- select(acura_best, model)
+}
+
+nested_best_acura_model <- function() {
+  best_acura_model <- select(
+    filter(
+      filter(
+        vehicles,
+        make == 'Acura',
+        year == 2015
+      ),
+      hwy == max(hwy)
+    ),
+    model
+  )
+}
+
+pipe_best_acura_model <- function() {
+  best_acura_model <- filter(vehicles, make == 'Acura', year == 2015) %>%
+    filter(hwy == max(hwy)) %>%
+    select(model)
+}
+
+system.time(for (i in 1:1000) temp_best_acura_model())
+system.time(for (i in 1:1000) nested_best_acura_model())
+system.time(for (i in 1:1000) pipe_best_acura_model())
